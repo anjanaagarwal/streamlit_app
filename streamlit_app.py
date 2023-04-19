c8bd291060fbff5970e44b2a67212fc8d5d78113
Anjana Agarwal <112549826+anjanaagarwal@users.noreply.github.com>
2023-04-19 21:24:16 +0530

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+def find_greatest(a, b, c):
+    if a > b and a > c:
+        return a
+    elif b > a and b > c:
+        return b
+    else:
+        return c
+
+def main():
+
+    st.title('Find the greatest among three numbers')
+    st.write('Enter three numbers below:')
+    a = st.number_input('Enter the first number:')
+    b = st.number_input('Enter the second number:')
+    c = st.number_input('Enter the third number:')
+    
+    if st.button('Find the greatest'):
+
+        answer = find_greatest(a, b, c)
+        st.success("The largest number is (answer)")
+
+
+if __name__ == "__main__":
+    main()
